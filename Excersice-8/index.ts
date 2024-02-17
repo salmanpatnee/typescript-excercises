f73850e6359eea70b341f1e01afd78a4461ceaa1
Salman <salmanpatni92@gmail.com>
2024-02-17 13:03:30 +0500

    Ex 8: Adding Comments

---
+++ Excersice-8/index.ts
@@ -0,0 +1,9 @@
+// Storing a favorite number and printing to the console.
+let favorite_number: number = 2;
+console.log(`Your favorite number is: ${favorite_number}`);
+
+// 4 arithmetic operations that result the number 8.
+console.log(7 + 1);
+console.log(9 - 1);
+console.log(2 * 4);
+console.log(8 / 1); 
