f3224c54b78e9a2c0d2ceee01129e8fb688731e9
Salman <salmanpatni92@gmail.com>
2024-02-17 12:47:12 +0500

    Ex 5: Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.

---
+++ Excersice-5/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let person_name = "\tSalman Abdul Ghani\n";
+//With the whitespace.
+console.log(person_name);
+//Without the whitespace.
+console.log(person_name.trim());
---
+++ Excersice-5/index.ts
@@ -0,0 +1,7 @@
+let person_name: string = "\tSalman Abdul Ghani\n";
+
+//With the whitespace.
+console.log(person_name);
+
+//Without the whitespace.
+console.log(person_name.trim())
