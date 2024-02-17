1b875a0ec93c67abae393bd471c18586185e131f
Salman <salmanpatni92@gmail.com>
2024-02-17 14:24:11 +0500

    Ex 11: Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples.

---
+++ Excercise-11/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let mode_of_transportations = ["Audi e-tron GT 2024", "Toyota Land Cruiser ZX", "Hyundai Sonata 2024"];
+mode_of_transportations.forEach(mode_of_transportation => {
+    console.log(`I would like to own a ${mode_of_transportation}.`);
+});
---
+++ Excercise-11/index.ts
@@ -0,0 +1,5 @@
+let mode_of_transportations: string[] = ["Audi e-tron GT 2024", "Toyota Land Cruiser ZX", "Hyundai Sonata 2024"];
+
+mode_of_transportations.forEach(mode_of_transportation => {
+    console.log(`I would like to own a ${mode_of_transportation}.`);
+});
