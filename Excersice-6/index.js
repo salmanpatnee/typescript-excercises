7cb0f63910af8fe9ee8dad01144b9081284d003f
Salman <salmanpatni92@gmail.com>
2024-02-17 12:52:37 +0500

    Ex 6:  Write addition, subtraction, multiplication, and division operations that each result in the number 8.

---
+++ Excersice-6/index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log(7 + 1);
+console.log(9 - 1);
+console.log(2 * 4);
+console.log(8 / 1);
---
+++ Excersice-6/index.ts
@@ -0,0 +1,4 @@
+console.log(7 + 1);
+console.log(9 - 1);
+console.log(2 * 4);
+console.log(8 / 1);
