403bb3def779cec3789ed3ce5d437dab4b98500e
Salman <salmanpatni92@gmail.com>
2024-02-17 12:30:21 +0500

    Ex 4: Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.

---
+++ Excercise-4/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_person = "Salman Patnee";
+let message = `once said, “Everyday is a learning day.”`;
+console.log(`${famous_person} ${message}`);
---
+++ Excercise-4/index.ts
@@ -0,0 +1,4 @@
+let famous_person: string = "Salman Patnee";
+let message: string = `once said, “Everyday is a learning day.”`; 
+
+console.log(`${famous_person} ${message}`)
