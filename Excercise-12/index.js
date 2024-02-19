3fba29dbef13065b00e66e8d3afe8e9ab55c7bb1
Salman <salmanpatni92@gmail.com>
2024-02-19 12:14:27 +0500

    Ex 12: Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite?

---
+++ Excercise-12/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let guests = ["John", "Mark", "Jane"];
+
+guests.forEach(guest => {
+    console.log(`${guest}, I would like to invite you on a dinner.`);
+});
---
+++ Excercise-12/index.ts
@@ -0,0 +1,5 @@
+let guests: string[] = ["John", "Mark", "Jane"];
+
+guests.forEach(guest => {
+    console.log(`${guest}, I would like to invite you on a dinner.`);
+});
