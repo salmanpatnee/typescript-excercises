bad3a44d7a2580575f5d1dd84ad9a0479f4a6963
Salman <salmanpatni92@gmail.com>
2024-02-29 14:00:28 +0500

    Ex 20: Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.

---
+++ Exercise-20/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let programming_languages = ["JavaScript", "PHP", "Python", "C#", "Ruby"];
+
+// Index is not defined
+console.log(`My favorite programming language is: ${programming_languages[5]}`);
+
+console.log(`My favorite programming language is: ${programming_languages[0]}`);
---
+++ Exercise-20/index.ts
@@ -0,0 +1,7 @@
+let programming_languages: string[] = ["JavaScript", "PHP", "Python", "C#", "Ruby"];
+
+
+// Index is not defined
+console.log(`My favorite programming language is: ${programming_languages[5]}`)
+
+console.log(`My favorite programming language is: ${programming_languages[0]}`)
