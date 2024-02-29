5ecd0b95fe6afb7d46f7aae73bf0aba5fa8a70f2
Salman <salmanpatni92@gmail.com>
2024-02-29 14:19:04 +0500

    Ex 21: Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:

---
+++ Exercise-21/index.ts
@@ -0,0 +1,54 @@
+// Test 1
+let day: string = "Thursday";
+
+console.log("day == 'Thursday' ? I predict True.")
+console.log(day == "Thursday")
+
+
+// Test 2
+let time: string = "2:10";
+
+console.log("time == '2:10' ? I predict True.");
+console.log(time == "2:10");
+
+
+// Test 3
+let mobile: string = "Samsung A32";
+
+console.log("mobile == 'Samsung A32' ? I predict True.");
+console.log(mobile == "Samsung A32");
+
+// Test 4
+let target: number = 150;
+
+console.log("target > 100 ? I predict True.");
+console.log(target > 100);
+
+// Test 5
+let karachi_won: boolean = true;
+
+console.log("karachi_won == True ? I predict True.");
+console.log(karachi_won == true);
+
+
+// Test 6
+console.log("day == 'Friday' ? I predict False.")
+console.log(day == "Friday")
+
+
+// Test 7
+console.log("time == '2:11' ? I predict False.");
+console.log(time == "2:11");
+
+// Test 8
+console.log("mobile == 'iPhone 14' ? I predict False.");
+console.log(mobile == "iPhone 14");
+
+
+// Test 9
+console.log("target == 100 ? I predict False.");
+console.log(target == 100);
+
+// Test 10
+console.log("karachi_won == false ? I predict False.");
+console.log(karachi_won = false);
