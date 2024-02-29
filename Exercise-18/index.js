4df62a91c75dda866cdde41ca619ddfd2b57babd
Salman <salmanpatni92@gmail.com>
2024-02-29 13:49:43 +0500

    Ex:18 List of programming languages.

---
+++ Exercise-18/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let programming_languages = ["JavaScript", "PHP", "Python", "C#", "Ruby"];
+
+programming_languages.forEach((programming_language, index) => {
+    console.log(`${index + 1}: ${programming_language}`);
+});
---
+++ Exercise-18/index.ts
@@ -0,0 +1,5 @@
+let programming_languages: string[] = ["JavaScript", "PHP", "Python", "C#", "Ruby"];
+
+programming_languages.forEach((programming_language, index) => {
+    console.log(`${index + 1}: ${programming_language}`)
+})
