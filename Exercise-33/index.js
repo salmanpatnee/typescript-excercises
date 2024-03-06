eb97ad5fa45ab8829f26950f77428bc7997a56e3
Salman <salmanpatni92@gmail.com>
2024-03-06 17:10:21 +0500

    Ex:33 Animals

---
+++ Exercise-33/index.js
@@ -0,0 +1,5 @@
+var animals = ['Parrot', "Gold fish", "Rabbit", "Cat", "Dog"];
+for (var index = 0; index < animals.length; index++) {
+    console.log("A ".concat(animals[index], " would make a friendly pet."));
+}
+console.log("These all animals are friendly pets.");
---
+++ Exercise-33/index.ts
@@ -0,0 +1,7 @@
+let animals: string[] = ['Parrot', "Gold fish", "Rabbit", "Cat", "Dog"];
+
+for (let index = 0; index < animals.length; index++) {
+    console.log(`A ${animals[index]} would make a friendly pet.`);
+}
+
+console.log(`These all animals are friendly pets.`);
