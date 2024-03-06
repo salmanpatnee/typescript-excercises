0d32a269d9483436c06c5be42398d728af8a3677
Salman <salmanpatni92@gmail.com>
2024-03-06 14:30:59 +0500

    Ex 23: Alien Colors #1

---
+++ Exercise-23/index.js
@@ -0,0 +1,10 @@
+var alien_color = "green";
+if (alien_color == "green") {
+    console.log("Hey! You just earned 5 points");
+}
+if (alien_color == "green") {
+    console.log("Hey! You just earned 5 points");
+}
+if (alien_color != "green") {
+    console.log("");
+}
---
+++ Exercise-23/index.ts
@@ -0,0 +1,13 @@
+let alien_color: string = "green";
+
+if(alien_color == "green"){
+    console.log("Hey! You just earned 5 points");
+}
+
+if(alien_color == "green"){
+    console.log("Hey! You just earned 5 points");
+}
+
+if(alien_color != "green"){
+    console.log("");
+}
