4f1016c511385cb225da7bb044cb43053cd9977e
Salman <salmanpatni92@gmail.com>
2024-03-06 17:10:03 +0500

    Ex-32: Pizzas

---
+++ Exercise-32/index.js
@@ -0,0 +1,8 @@
+var pizzas = ["Fajita Pizza", "Arabic ranch pizza", "Sicilian pizza"];
+for (var index = 0; index < pizzas.length; index++) {
+    console.log("I like ".concat(pizzas[index]));
+}
+console.log("I really like the ".concat(pizzas[0], " because it is so falvorful."));
+console.log("I really like the ".concat(pizzas[1], " because it is not spicy but very saucy"));
+console.log("I really like the ".concat(pizzas[2], " because it is spicy."));
+console.log(" I really love pizza!");
---
+++ Exercise-32/index.ts
@@ -0,0 +1,11 @@
+let pizzas: string[] = ["Fajita Pizza", "Arabic ranch pizza", "Sicilian pizza"];
+
+for (let index = 0; index < pizzas.length; index++) {
+    console.log(`I like ${pizzas[index]}`);
+}
+
+console.log(`I really like the ${pizzas[0]} because it is so falvorful.`)
+console.log(`I really like the ${pizzas[1]} because it is not spicy but very saucy`)
+console.log(`I really like the ${pizzas[2]} because it is spicy.`)
+
+console.log(" I really love pizza!");
