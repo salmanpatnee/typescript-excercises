ee86fc621040ea5427dfc4fe42ec8f6b78d2a09a
Salman <salmanpatni92@gmail.com>
2024-03-06 15:23:50 +0500

    Ex 25: Alien Colors #3

---
+++ Exercise-25/index.js
@@ -0,0 +1,10 @@
+var alien_color = "red";
+if (alien_color == "green") {
+    console.log("Hey! You just earned 5 points");
+}
+else if (alien_color == "yellow") {
+    console.log("Hey! You just earned 10 points");
+}
+else if (alien_color == "red") {
+    console.log("Hey! You just earned 15 points");
+}
---
+++ Exercise-25/index.ts
@@ -0,0 +1,11 @@
+let alien_color: string = "red";
+
+if(alien_color == "green"){
+    console.log("Hey! You just earned 5 points");
+} 
+else if(alien_color == "yellow"){
+    console.log("Hey! You just earned 10 points");
+}
+else if(alien_color == "red"){
+    console.log("Hey! You just earned 15 points");
+}
