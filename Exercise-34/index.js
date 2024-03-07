865e521ebf6a7e2f7266f29ee1aff07525241968
Salman <salmanpatni92@gmail.com>
2024-03-07 14:13:45 +0500

    Ex-34:T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.

---
+++ Exercise-34/index.js
@@ -0,0 +1,4 @@
+function make_shirt(size, message) {
+    return "Size of the shirt is ".concat(size, " and \"").concat(message, "\" message is printed on the shirt");
+}
+console.log(make_shirt("Large", "Catch me if you can!"));
---
+++ Exercise-34/index.ts
@@ -0,0 +1,5 @@
+function make_shirt(size: string, message: string): string {
+    return `Size of the shirt is ${size} and "${message}" message is printed on the shirt`;
+}
+
+console.log(make_shirt("Large", "Catch me if you can!"));
