c6271973c51deb17a29cafe2652c137ddd27757c
Salman <salmanpatni92@gmail.com>
2024-03-07 16:18:01 +0500

    Ex:39 Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array..

---
+++ Exercise-39/index.js
@@ -0,0 +1,7 @@
+var magicians_names = ["Albus Dumbledore", "Harry Porter", "Severus Snape", "Sirius Black"];
+function show_magicians(names) {
+    names.map(function (name) {
+        console.log(name);
+    });
+}
+show_magicians(magicians_names);
---
+++ Exercise-39/index.ts
@@ -0,0 +1,11 @@
+let magicians_names: string[] = ["Albus Dumbledore", "Harry Porter", "Severus Snape", "Sirius Black"];
+
+function show_magicians(names: string[]): void {
+    names.map(name => {
+        console.log(name);
+        
+    })
+}
+
+
+show_magicians(magicians_names);
