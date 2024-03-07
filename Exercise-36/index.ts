1429d5adee800b2c9145b6bca3ff8c1b8980410f
Salman <salmanpatni92@gmail.com>
2024-03-07 14:21:33 +0500

    Ex:36 Cities: Write a function called describe_city() that accepts the name of a city and its country. The function should print a simple sentence, such as Karachi is in Pakistan. Give the parameter for the country a default value. Call your function for three different cities, at least one of which is not in the default country.

---
+++ Exercise-36/index.ts
@@ -0,0 +1,7 @@
+function describe_city(city: string, country: string = "Pakistan"): string {
+    return `${city} is in ${country}`;
+}
+
+console.log(describe_city("Karachi"));
+console.log(describe_city("Lahore"));
+console.log(describe_city("Newyork", "America"));
