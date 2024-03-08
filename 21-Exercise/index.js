5495c1d78c33c067a8c5ad9731a42e92da6c55fd
Salman <salmanpatni92@gmail.com>
2024-03-08 15:46:15 +0500

    Add complited .js file for ex 21

---
+++ 21-Exercise/index.js
@@ -0,0 +1,35 @@
+// Test 1
+var day = "Thursday";
+console.log("day == 'Thursday' ? I predict True.");
+console.log(day == "Thursday");
+// Test 2
+var time = "2:10";
+console.log("time == '2:10' ? I predict True.");
+console.log(time == "2:10");
+// Test 3
+var mobile = "Samsung A32";
+console.log("mobile == 'Samsung A32' ? I predict True.");
+console.log(mobile == "Samsung A32");
+// Test 4
+var target = 150;
+console.log("target > 100 ? I predict True.");
+console.log(target > 100);
+// Test 5
+var karachi_won = true;
+console.log("karachi_won == True ? I predict True.");
+console.log(karachi_won == true);
+// Test 6
+console.log("day == 'Friday' ? I predict False.");
+console.log(day == "Friday");
+// Test 7
+console.log("time == '2:11' ? I predict False.");
+console.log(time == "2:11");
+// Test 8
+console.log("mobile == 'iPhone 14' ? I predict False.");
+console.log(mobile == "iPhone 14");
+// Test 9
+console.log("target == 100 ? I predict False.");
+console.log(target == 100);
+// Test 10
+console.log("karachi_won == false ? I predict False.");
+console.log(karachi_won = false);
